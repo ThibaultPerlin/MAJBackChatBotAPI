6ba4991489818918f9c44b8ec8108dd3365fea6d
ThibaultPerlin <103318328+ThibaultPerlin@users.noreply.github.com>
2023-06-08 11:25:59 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+var mysql = require('mysql');
+const port = 3000
+var con = mysql.createConnection({
+  host: "sql7.freesqldatabase.com",
+  user: "sql7623778",
+  password: "YFDKuZsxFI"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+/*const mongoose =require("mongoose")
+*/
+
+
+
+// Swagger
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Dialog API',
+            description: 'Dialog API Information',
+            contact: {
+                name: 'Developer Name',
+            },
+            servers: ['http://localhost:3000'],
+        },
+    },
+    apis: ['./routes/v1/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use('/api/v1', require('./routes/v1'))
+
+app.get('*', (req, res) => {
+    // res.status(404).json({message: 'Not found'}) bonne pratique
+    res.sendFile(__dirname + '/view/404.html')
+})
+
+
+   /*  mongoose.connect('mongodb+srv://admin:admin@stjo.n7hiept.mongodb.net/')
+     .then(console.log("connected to database"))
+     .catch((error)=>{console.log("error")});*/
+  
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
